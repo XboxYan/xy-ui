3d4356cab598e5be4cf53c4781cc14ea4d643810
XboxYan <yanwenbin1991@live.com>
2019-06-05 18:22:17 +0800

    add keyboard

--- components/xy-button.js
+++ components/xy-button.js
@@ -1,15 +1,24 @@
 export default class XyButton extends HTMLElement {
+
+    static get observedAttributes() { return ['disabled'] }
+
     constructor() {
         super();
         const shadowRoot = this.attachShadow({ mode: 'open' });
         shadowRoot.innerHTML = `
         <style>
-        :host{ display:inline-block; line-height: 2; font-size: 14px; background: dodgerblue; color: #fff;  border-radius: 3px; }
-        :host([disabled]){ pointer-events:none; opacity:.8; }
-        .btn{ position: relative; padding:0 .8em; transition: .3s; user-select: none; text-align: center; }
-        .btn>label{ position: absolute; left: 0; top: 0; width: 100%; height: 100%; overflow:hidden; cursor: pointer; }
-        .btn>label:after{ content: ''; position: absolute; background: transparent; border-radius:50%; width: 100%; padding-top: 100%; margin-left: -50%; margin-top: -50%; left: var(--x,-100%); top: var(--y,-100%); background:#fff; opacity:.3 }
-        .btn>input[type=checkbox]{ position:absolute; clip:rect(0,0,0,0)}
+        :host{ display:inline-block; line-height: 2.4; border:1px solid #ccc; font-size: 14px; color: #333;  border-radius: 3px; transition:background .3s,box-shadow .3s,border-color .3s,color .3s; transform: translateZ(0);}
+        :host([disabled]){ pointer-events:none; opacity:.8; cursor: not-allowed; }
+        :host(:not([type="primary"]):hover),:host(:not([type="primary"]):focus-within){ color:var(--themeColor,dodgerblue); border-color: var(--themeColor,dodgerblue); }
+        :host(:not([type="primary"])) .btn>label:after{ background: var(--themeColor,dodgerblue); }
+        :host([type="primary"]){ border-color: var(--themeColor,dodgerblue); color: #fff; background: var(--themeColor,dodgerblue) }
+        :host([type="dashed"]){ border-style:dashed }
+        :host(:focus-within){ box-shadow:0 0 3px var(--themeColor,dodgerblue) }
+        .btn{ display:block; color: inherit; line-height: inherit; font-size: inherit;  background:none; outline:0; border:0; position: relative; padding:0 .8em; user-select: none; text-align: center; }
+        ::-moz-focus-inner{border:0;}
+        .btn>label{ position: absolute; left: 0; top: 0; padding:0; width: 100%; height: 100%; overflow:hidden; cursor: pointer; outline:0; }
+        .btn>label:after{ content: ''; pointer-events:none; position: absolute; background: transparent; border-radius:50%; width: 100%; padding-top: 100%; margin-left: -50%; margin-top: -50%; left: var(--x,-100%); top: var(--y,-100%); background:#fff; opacity:.3 }
+        .btn>input[type=checkbox]{position:absolute; clip:rect(0,0,0,0)}
         .btn>input[type=checkbox]+label:after{animation: ripple-in .5s forwards;}
         .btn>input[type=checkbox]:checked+label:after{animation: ripple-out .5s forwards;}
         @keyframes ripple-in{
@@ -41,16 +50,44 @@ export default class XyButton extends HTMLElement {
             }
         }
         </style>
-        <div class="btn"><input type="checkbox" id="checkbox"><label for="checkbox" id="label"></label><slot></slot></div>
+        <button class="btn" id="btn" ${this.disabled==""?"disabled":""} tabindex="1"><input type="checkbox" tabindex="-1" id="checkbox"><label for="checkbox" id="label" tabindex="-1"></label><slot></slot></button>
         `
-    } 
+    }
+
+    get disabled() {
+        return this.getAttribute('disabled');
+    }
     
     connectedCallback() {
-        this.shadowRoot.getElementById('label').addEventListener('click',function(ev){
+        const _this = this;
+        this.btn = this.shadowRoot.getElementById('btn');
+        this.label = this.shadowRoot.getElementById('label');
+        this.checkbox = this.shadowRoot.getElementById('checkbox');
+        this.label.addEventListener('mousedown',function(ev){
+            ev.preventDefault();
             ev.stopPropagation();
             const { offsetX, offsetY } = ev;
             this.style.setProperty('--x',offsetX+'px');
             this.style.setProperty('--y',offsetY+'px');
         })
+        this.checkbox.addEventListener('click',function(ev){
+            _this.btn.focus();
+            ev.stopPropagation();
+        })
+        this.btn.addEventListener('click',function(ev){
+            ev.stopPropagation();
+            _this.dispatchEvent(new CustomEvent('click'));
+        })
+
+    }
+
+    attributeChangedCallback (name, oldValue, newValue) {
+        if( name == 'disabled' && this.btn){
+            if(newValue==""){
+                this.btn.setAttribute('disabled', 'disabled');
+            }else{
+                this.btn.removeAttribute('disabled');
+            }
+        }
     }
 }
--- components/xy-slider.js
+++ components/xy-slider.js
@@ -1,55 +1,86 @@
 export default class XySlider extends HTMLElement {
 
-    static get observedAttributes() { return ['value'] }
+    static get observedAttributes() { return ['value','min','max','step','disabled'] }
 
     constructor() {
         super();
         const shadowRoot = this.attachShadow({ mode: 'open' });
         shadowRoot.innerHTML = `
         <style>
-        :host{ display:block;  }
-        :host([disabled]){ pointer-events:none; opacity:.8; }
+        :host{ display:flex; padding:5px 0; }
+        :host([disabled]){ opacity:.8; --themeColor:#999}
+        ::-moz-focus-inner,::-moz-focus-outer{border:0;outline : 0;}
         input[type="range"]{
+            width:100%;
             -webkit-appearance: none;
-            outline : none;
+            outline : 0;
+            background: rgba(0,0,0,.1);
+            height: 4px;
+            border-radius:2px;
         }
         input[type="range"]::-webkit-slider-runnable-track{
             display: flex;
             align-items: center;
             position: relative;
             height: 4px;
-            background: #f22;
             border-radius:2px;
-            background:var(--sliderColor,dodgerblue);
+            background:linear-gradient(to right, var(--themeColor,dodgerblue) calc(100% * var(--percent)), transparent 0% )
+        }
+        input[type="range"]::-moz-range-progress {
+            display: flex;
+            align-items: center;
+            position: relative;
+            height: 4px;
+            border-radius:2px;
+            outline : 0;
+            background:var(--themeColor,dodgerblue)
         }
         input[type="range"]::-webkit-slider-thumb{
             -webkit-appearance: none;
+            border:0;
             position: relative;
             width:10px;
             height:10px;
             border-radius: 50%;
-            background:var(--sliderColor,dodgerblue);
+            background:var(--themeColor,dodgerblue);
             transition:.2s;
-          }
-        input[type="range"]::-webkit-slider-thumb:active{
-            transform:scale(1.2)
-          }
+        }
+        input[type="range"]::-moz-range-thumb{
+            pointer-events:none;
+            border:0;
+            position: relative;
+            width:10px;
+            height:10px;
+            border-radius: 50%;
+            background:var(--themeColor,dodgerblue);
+            transition:.2s;
+        }
+        input[type="range"]::-webkit-slider-thumb:active,
+        input[type="range"]:focus::-webkit-slider-thumb{
+            transform:scale(1.2);
+            box-shadow:0 0 3px var(--themeColor,dodgerblue)
+        }
+        input[type="range"]::-moz-range-thumb:active,
+        input[type="range"]:focus::-moz-range-thumb{
+            transform:scale(1.2);
+            box-shadow:0 0 3px var(--themeColor,dodgerblue)
+        }
         </style>
-        <input id='slider' type='range'>
+        <input id='slider' value=${this.value} min=${this.min} max=${this.max} step=${this.step} ${this.disabled==""?"disabled":""} type='range'>
         `
-        //console.log(this.parentNode.index);
     } 
     
     connectedCallback() {
         const _this = this;
         this.slider = this.shadowRoot.getElementById('slider');
-        this.slider.value = this.value;
         this.slider.addEventListener('input',function(ev){
             _this.value = this.value;
             _this._oninput = true;
         })
         this.slider.addEventListener('change',function(ev){
+            _this.value = this.value;
             _this._oninput = false;
+            _this.dispatchEvent(new CustomEvent('change'));
         })
     }
 
@@ -57,15 +88,50 @@ export default class XySlider extends HTMLElement {
         return this.getAttribute('value')||0;
     }
 
+    get min() {
+        return this.getAttribute('min')||0;
+    }
+
+    get max() {
+        return this.getAttribute('max')||100;
+    }
+
+    get disabled() {
+        return this.getAttribute('disabled');
+    }
+
+    get step() {
+        return this.getAttribute('step')||1;
+    }
+
     set value(value) {
         this.setAttribute('value', value);
-        this.dispatchEvent(new CustomEvent('oninput', { detail: value }));
+    }
+
+    set min(value) {
+        this.setAttribute('min', value);
+    }
+
+    set max(value) {
+        this.setAttribute('max', value);
+    }
+
+    set step(value) {
+        this.setAttribute('step', value);
     }
 
     attributeChangedCallback (name, oldValue, newValue) {
-        if(name=='value'){
-            if(oldValue && oldValue!==newValue && !this._oninput){
-                this.slider.value = newValue;
+        this.style.setProperty('--percent',(this.value-this.min)/(this.max-this.min));
+        if( this.slider && oldValue!==newValue && !this._oninput){
+            if(name == 'disabled'){
+                if(newValue==""){
+                    this.slider.setAttribute('disabled', 'disabled');
+                }else{
+                    this.slider.removeAttribute('disabled');
+                }
+            }else{
+                this.slider[name] = newValue;
+                this[name] = newValue;
             }
         }
     }
