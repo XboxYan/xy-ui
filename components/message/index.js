649ba720f1487533e4f8f990ce6ef8d9e5c92963
XboxYan <yanwenbin1991@live.com>
2023-03-30 00:05:00 +0800

    fix: tab

--- components/message/index.js
+++ components/message/index.js
@@ -5,9 +5,9 @@ import "../button/index.js";
 import style from "./index.css?inline" assert { type: "css" };
 
 class XyMessage extends Base {
-  #loadEl;
-  #icon;
-  #messageContent;
+	#loadEl;
+	#icon;
+	#messageContent;
 
 	static get observedAttributes() {
 		return ["type", "icon", "loading"];
@@ -21,15 +21,15 @@ class XyMessage extends Base {
       <xy-icon id="icon"></xy-icon>
       <slot></slot>
         `;
-    this.#icon = shadowRoot.getElementById('icon');
-    this.#messageContent = document.getElementById("message-content");
-    if (!this.#messageContent) {
-      this.#messageContent = document.createElement("div");
-      this.#messageContent.id = "message-content";
-      this.#messageContent.style =
-        "position:fixed; pointer-events:none; left:0; right:0; top:10px; z-index:51;";
-      document.body.appendChild(this.#messageContent);
-    }
+		this.#icon = shadowRoot.getElementById("icon");
+		this.#messageContent = document.getElementById("message-content");
+		if (!this.#messageContent) {
+			this.#messageContent = document.createElement("div");
+			this.#messageContent.id = "message-content";
+			this.#messageContent.style =
+				"position:fixed; pointer-events:none; left:0; right:0; top:10px; z-index:51;";
+			document.body.appendChild(this.#messageContent);
+		}
 	}
 
 	get open() {
@@ -83,39 +83,39 @@ class XyMessage extends Base {
 		},
 	};
 
-  render() {
-    this.#messageContent.appendChild(this)
-    this.clientWidth;
-    this.open = true
-  }
+	render() {
+		this.#messageContent.appendChild(this);
+		this.clientWidth;
+		this.open = true;
+	}
 
 	connectedCallback() {
 		this.addEventListener("transitionend", (ev) => {
 			if (ev.propertyName === "transform" && !this.open) {
-        this.dispatchEvent(new Event("close"));
-				this.remove()
+				this.dispatchEvent(new Event("close"));
+				this.remove();
 			}
 		});
 	}
 
 	attributeChangedCallback(name, oldValue, newValue) {
 		if (name == "type") {
-      this.#icon.name = this.#typeMap[newValue].name;
-      this.#icon.color = this.#typeMap[newValue].color;
+			this.#icon.name = this.#typeMap[newValue].name;
+			this.#icon.color = this.#typeMap[newValue].color;
 		}
 		if (name == "icon") {
-      this.#icon.name = newValue;
+			this.#icon.name = newValue;
 		}
 		if (name == "loading") {
-      if (!this.#loadEl) {
-        this.#loadEl = document.createElement("xy-loading");
-      }
-      this.loading = newValue!==null;
-      if (newValue !== null) {
-        this.shadowRoot.prepend(this.#loadEl)
-      } else {
-        this.shadowRoot.removeChild(this.#loadEl)
-      }
+			if (!this.#loadEl) {
+				this.#loadEl = document.createElement("xy-loading");
+			}
+			this.loading = newValue !== null;
+			if (newValue !== null) {
+				this.shadowRoot.prepend(this.#loadEl);
+			} else {
+				this.shadowRoot.removeChild(this.#loadEl);
+			}
 		}
 	}
 }
@@ -124,61 +124,56 @@ if (!customElements.get("xy-message")) {
 	customElements.define("xy-message", XyMessage);
 }
 
-export const open = (
-  type, 
-  text, 
-  duration, 
-  onclose, 
-) => {
-  const message = new XyMessage();
-  message.timer && clearTimeout(message.timer);
-  if (type === 'loading') {
-    message.loading = true
-  } else {
-    message.type = type;
-  }
-  message.textContent = text || "";
-  message.render()
-  if (typeof duration === 'function') {
-    duration()
-  } else {
-    message.onclose = onclose;
-    if (duration !== 0 && !message.loading) {
-      message.timer = setTimeout(() => {
-        message.open = false;
-      }, duration || 3000);
-    }
-  }
-  return message;
-}
+export const open = (type, text, duration, onclose) => {
+	const message = new XyMessage();
+	message.timer && clearTimeout(message.timer);
+	if (type === "loading") {
+		message.loading = true;
+	} else {
+		message.type = type;
+	}
+	message.textContent = text || "";
+	message.render();
+	if (typeof duration === "function") {
+		duration();
+	} else {
+		message.onclose = onclose;
+		if (duration !== 0 && !message.loading) {
+			message.timer = setTimeout(() => {
+				message.open = false;
+			}, duration || 3000);
+		}
+	}
+	return message;
+};
 
 export const info = (...params) => {
-  return open('info',...params)
-}
+	return open("info", ...params);
+};
 
 export const success = (...params) => {
-  return open('success',...params)
-}
+	return open("success", ...params);
+};
 export const warning = (...params) => {
-  return open('warning',...params)
-}
+	return open("warning", ...params);
+};
 export const error = (...params) => {
-  return open('error',...params)
-}
+	return open("error", ...params);
+};
 export const loading = (...params) => {
-  return open('loading',...params)
-}
+	return open("loading", ...params);
+};
 export const show = ({
-  type='success',
-  icon,
-  text,
-  duration = 3000,
-  onclose
+	type = "success",
+	icon,
+	text,
+	duration = 3000,
+	onclose,
 }) => {
-  const message = open(type, text, duration, onclose)
-  message.icon = icon
-  return message
-}
+	const message = open(type, text, duration, onclose);
+	message.icon = icon;
+	return message;
+};
 
 export default {
 	info,
